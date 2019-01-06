671cebcc9494de3c1d8d1c4b68c3b1cdab59a6af
Vartika06 <magicalvartika06@gmail.com>
2019-01-06 23:22:05 +0530

    Add project files.

---
+++ GameConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sarah = new PlayerCharacter(new DiamondSkinDefence())
+            {
+                Name = "Sarah"            
+            };
+
+            var amrit = new PlayerCharacter(SpecialDefence.Null)
+            {
+                Name = "Amrit"
+            };
+
+            var gentry = new PlayerCharacter(SpecialDefence.Null)
+            {
+                Name = "Gentry"
+            };
+
+            sarah.Hit(10);
+            amrit.Hit(10);
+            gentry.Hit(10);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GameConsole/SpecialDefence.cs
@@ -0,0 +1,17 @@
+﻿namespace GameConsole
+{
+    public abstract class SpecialDefence
+    {
+        public abstract int CalculateDamageReduction(int totalDamage);
+
+        public static SpecialDefence Null { get; } = new NullDefense();
+
+        private class NullDefense : SpecialDefence
+        {
+            public override int CalculateDamageReduction(int totalDamage)
+            {
+                return 0; // no operation / " do nothing" behaviour
+            }
+        }
+    }
+}
